e11ef6928799b9e00c7de6d6aabab00b9f420690
Joy Goswami <joy63586@gmail.com>
2024-06-10 23:19:48 +0600

    everything is working except %

--- script.js
+++ script.js
@@ -9,46 +9,100 @@ btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     if (e.target.classList.contains("btn-num")) {
       if (e.target.textContent === "0") {
-        setResultDisplayArr("0");
+        setExpressionDisplayArr("0");
       } else if (e.target.textContent === "1") {
-        setResultDisplayArr("1");
+        setExpressionDisplayArr("1");
       } else if (e.target.textContent === "2") {
-        setResultDisplayArr("2");
+        setExpressionDisplayArr("2");
       } else if (e.target.textContent === "3") {
-        setResultDisplayArr("3");
+        setExpressionDisplayArr("3");
       } else if (e.target.textContent === "4") {
-        setResultDisplayArr("4");
+        setExpressionDisplayArr("4");
       } else if (e.target.textContent === "5") {
-        setResultDisplayArr("5");
+        setExpressionDisplayArr("5");
       } else if (e.target.textContent === "6") {
-        setResultDisplayArr("6");
+        setExpressionDisplayArr("6");
       } else if (e.target.textContent === "7") {
-        setResultDisplayArr("7");
+        setExpressionDisplayArr("7");
       } else if (e.target.textContent === "8") {
-        setResultDisplayArr("8");
+        setExpressionDisplayArr("8");
       } else if (e.target.textContent === "9") {
-        setResultDisplayArr("9");
+        setExpressionDisplayArr("9");
       } else if (e.target.textContent === ".") {
-        setResultDisplayArr(".");
+        setExpressionDisplayArr(".");
       }
-    } else if (e.target.classList.contains("btn-operator")) {
-      if (resultDisplayArr.length > 0) {
+    } else if (
+      e.target.classList.contains("btn-operator") &&
+      expressionDisplayArr.length > 0
+    ) {
+      const lastElement = expressionDisplayArr[expressionDisplayArr.length - 1];
+      if (
+        lastElement != "+" &&
+        lastElement != "-" &&
+        lastElement != "/" &&
+        lastElement != "*"
+      ) {
         if (e.target.classList.contains("btn-operator-addition")) {
-          let resultDisplayValue = resultDisplayArr.join("");
-          setExpressionDisplayArr(resultDisplayValue, "+");
+          setExpressionDisplayArr("+");
+        } else if (e.target.classList.contains("btn-operator-subtraction")) {
+          setExpressionDisplayArr("-");
+        } else if (e.target.classList.contains("btn-operator-division")) {
+          setExpressionDisplayArr("/");
+        } else if (e.target.classList.contains("btn-operator-multiplication")) {
+          setExpressionDisplayArr("*");
         }
+      }
+    } else if (e.target.textContent === "AC") {
+      expressionDisplay.textContent = "";
+      resultDisplay.textContent = "";
+      expressionDisplayArr = [];
+    } else if (e.target.textContent === "=") {
+      let result = eval(expressionDisplayArr.join(""));
+      expressionDisplayArr = [];
+      setExpressionDisplayArr(result);
+      expressionDisplay.textContent = "";
+    } else if (e.target.textContent === "←") {
+      if (expressionDisplayArr.length > 1) {
+        expressionDisplayArr.pop();
+        expressionDisplay.textContent = expressionDisplayArr.join("");
+      } else {
+        expressionDisplayArr = [];
+        expressionDisplay.textContent = "";
+        resultDisplay.textContent = "";
       }
     }
   });
 });
 
-function setResultDisplayArr(value) {
-  resultDisplayArr.push(value);
-  resultDisplay.textContent = resultDisplayArr.join("");
-}
-function setExpressionDisplayArr(resultDisplayValue, value) {
-  expressionDisplayArr.push(resultDisplayValue);
+function setExpressionDisplayArr(value) {
   expressionDisplayArr.push(value);
-  expressionDisplay.textContent = expressionDisplayArr.join(" ");
-  resultDisplayArr = [];
+  expressionDisplay.textContent = expressionDisplayArr.join("");
+  let expressionStr = expressionDisplayArr.join("");
+  setResultDisplay(expressionStr);
+}
+// function setExpressionDisplayArr(resultDisplayValue, value) {
+//   expressionDisplayArr.push(resultDisplayValue);
+//   expressionDisplayArr.push(value);
+//   expressionDisplay.textContent = expressionDisplayArr.join(" ");
+//   resultDisplayArr = [];
+// }
+
+function setResultDisplay(value) {
+  console.log(value);
+  if (
+    value.includes("+") ||
+    value.includes("-") ||
+    value.includes("*") ||
+    value.includes("/")
+  ) {
+    if (
+      value.charAt(value.length - 1) != "+" &&
+      value.charAt(value.length - 1) != "-" &&
+      value.charAt(value.length - 1) != "*" &&
+      value.charAt(value.length - 1) != "/"
+    ) {
+      console.log(eval(value));
+      resultDisplay.textContent = eval(value);
+    }
+  }
 }
