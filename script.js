6adebf079772b63e321fb925d698ba08c247feac
Joy Goswami <joy63586@gmail.com>
2024-06-11 18:01:51 +0600

    everything is working, still need to work on responsibility

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ const resultDisplay = document.querySelector(".result-display");
 const expressionDisplay = document.querySelector(".expression-display");
 
 let resultDisplayArr = [];
+// it takes each button input in an array format
 let expressionDisplayArr = [];
+// hasDecimalPoint is used for the placement of decimalpoint (.) value
+// when it is true we can add (.) in expressionDisplayArr ie. expressionDisplay
+// after pressing (.) btn hasDecimalPoint becomes false
+// when we press math operator like + - / * hasDecimalPoint becomes true again
+// again it becomes false when we press (.) button
+let hasDecimalPoint = true;
 
+// adds en addEventListener to each button
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
+    console.log(e.target);
     if (e.target.classList.contains("btn-num")) {
       if (e.target.textContent === "0") {
         setExpressionDisplayArr("0");
@@ -29,9 +38,19 @@ btns.forEach((btn) => {
       } else if (e.target.textContent === "9") {
         setExpressionDisplayArr("9");
       } else if (e.target.textContent === ".") {
-        setExpressionDisplayArr(".");
+        // if hasDecimalPoint is true then we can add (.) to expressionDisplayArr and expressionDisplay
+        // if it if false we can't
+        // after pressing (.) hasDecimalPoint becomes false. it prevent us from typing it TWICE
+        if (hasDecimalPoint) {
+          setExpressionDisplayArr(".");
+          hasDecimalPoint = false;
+        }
       }
-    } else if (
+    }
+    // if user presses any btn-operator buttons and expressionDisplayArr has elements i.e. numbers
+    // and expressionDisplayArr doesn't contains any math operators in its last index
+    // than we can push math operators to expressionDisplayArr array.
+    else if (
       e.target.classList.contains("btn-operator") &&
       expressionDisplayArr.length > 0
     ) {
@@ -42,6 +61,7 @@ btns.forEach((btn) => {
         lastElement != "/" &&
         lastElement != "*"
       ) {
+        hasDecimalPoint = true;
         if (e.target.classList.contains("btn-operator-addition")) {
           setExpressionDisplayArr("+");
         } else if (e.target.classList.contains("btn-operator-subtraction")) {
@@ -52,20 +72,42 @@ btns.forEach((btn) => {
           setExpressionDisplayArr("*");
         }
       }
-    } else if (e.target.textContent === "AC") {
+    }
+    // if user press AC button it makes hasDecimalPoint true
+    // empty two displays and expressionDisplayArr array
+    else if (e.target.textContent === "AC") {
+      hasDecimalPoint = true;
       expressionDisplay.textContent = "";
       resultDisplay.textContent = "";
       expressionDisplayArr = [];
     } else if (e.target.textContent === "=") {
-      let result = eval(expressionDisplayArr.join(""));
-      expressionDisplayArr = [];
-      setExpressionDisplayArr(result);
+      try {
+        let result = eval(expressionDisplayArr.join(""));
+        expressionDisplayArr = [];
+        setExpressionDisplayArr(result);
+      } catch (error) {
+        console.log("error is: " + error);
+      }
+      // let result = eval(expressionDisplayArr.join(""));
+      // expressionDisplayArr = [];
+      // setExpressionDisplayArr(result);
       expressionDisplay.textContent = "";
     } else if (e.target.textContent === "←") {
       if (expressionDisplayArr.length > 1) {
         expressionDisplayArr.pop();
         expressionDisplay.textContent = expressionDisplayArr.join("");
+        setResultDisplay(expressionDisplayArr.join(""));
       } else {
+        expressionDisplayArr = [];
+        expressionDisplay.textContent = "";
+        resultDisplay.textContent = "";
+      }
+    } else if (e.target.classList.contains("btn-operator-plus-minus")) {
+      if (expressionDisplayArr.length == 0) {
+        setExpressionDisplayArr("-");
+        return;
+      }
+      if (expressionDisplayArr.length == 1 && expressionDisplayArr[0] == "-") {
         expressionDisplayArr = [];
         expressionDisplay.textContent = "";
         resultDisplay.textContent = "";
@@ -101,8 +143,14 @@ function setResultDisplay(value) {
       value.charAt(value.length - 1) != "*" &&
       value.charAt(value.length - 1) != "/"
     ) {
-      console.log(eval(value));
-      resultDisplay.textContent = eval(value);
+      // console.log(eval(value));
+      try {
+        let sum = eval(value);
+        resultDisplay.textContent = sum;
+      } catch (error) {
+        console.log("error is: " + error);
+      }
+      // resultDisplay.textContent = eval(value);
     }
   }
 }
